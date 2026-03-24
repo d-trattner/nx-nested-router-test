5d268e2b445faedabfc778503ec5f350e72733af
Daniel <daniel.trattner@gmail.com>
2026-03-24 14:58:56 +0100

    Update app.config.ts

--- apps/test/src/app/app.config.ts
+++ apps/test/src/app/app.config.ts
@@ -3,13 +3,12 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
-import { provideMockStore } from '@ngrx/store/testing';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideEffects(),
-    provideStore(),
-    provideMockStore({}),
+    provideEffects({}),
+    provideStore({}),
+    // provideMockStore({}),
     provideRouter(appRoutes, withComponentInputBinding()),
 
   ],
